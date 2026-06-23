9ec6b01482a36746fff5afc12874d87df1df66df
YatsivDEN <stefanijul11@gmail.com>
2026-06-23 15:43:55 +0300

    Done calculateRentalCost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const price = 40;
+  const basePrice = price * days;
+  const longTerm = 7;
+  const longTermDiscount = 50;
+  const midTerm = 3;
+  const midTermDiscount = 20;
+
+  if (days >= longTerm) {
+    return basePrice - longTermDiscount;
+  } else if (days >= midTerm) {
+    return basePrice - midTermDiscount;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
